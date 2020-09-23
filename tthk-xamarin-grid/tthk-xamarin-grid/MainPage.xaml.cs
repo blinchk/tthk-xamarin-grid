a199c0fbb02b2ed162b3901333ce2a08c5d685ee
Nikolas Laus <nikolas.laus@protonmail.com>
2020-09-23 13:02:20 +0300

    Add boxes in Grid5x5 with TapGestureRecognizer.

--- tthk-xamarin-grid/tthk-xamarin-grid/MainPage.xaml.cs
+++ tthk-xamarin-grid/tthk-xamarin-grid/MainPage.xaml.cs
@@ -10,9 +10,44 @@ namespace tthk_xamarin_grid
 {
     public partial class MainPage : ContentPage
     {
+        BoxView boxView;
         public MainPage()
         {
-            Grid grid = new Grid();
+            Grid grid = new Grid() {
+                RowDefinitions =
+                {
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) }
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition {Width = new GridLength(1,GridUnitType.Star) },
+                    new ColumnDefinition {Width = new GridLength(1,GridUnitType.Star) },
+                    new ColumnDefinition {Width = new GridLength(1,GridUnitType.Star) },
+                    new ColumnDefinition {Width = new GridLength(1,GridUnitType.Star) },
+                    new ColumnDefinition {Width = new GridLength(1,GridUnitType.Star) }
+                }
+            };
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    boxView = new BoxView { Color = Color.FromHex("#0099FF") };
+                    grid.Children.Add(boxView, i, j);
+                }
+            }
+
+            var tap = new TapGestureRecognizer();
+            tap.Tapped += BoxTapped;
+            Content = grid;
+        }
+
+        private void BoxTapped(object sender, EventArgs e)
+        {
         }
     }
 }
