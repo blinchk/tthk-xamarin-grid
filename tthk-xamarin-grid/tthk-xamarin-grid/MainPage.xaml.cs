5e545cf7631f628a9d1a75d2b1ce5c88d8e27f30
Opilane TTHK <opilane@tthk.ee>
2020-09-23 12:43:21 +0300

    Add project files.

---
+++ tthk-xamarin-grid/tthk-xamarin-grid/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace tthk_xamarin_grid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Grid grid = new Grid();
+        }
+    }
+}
